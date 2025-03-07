e2481bcd8734ac46d8824dfceddd54c5aa084d71
sam-a-d <masamad.sust@gmail.com>
2025-03-07 12:10:40 +0200

    updated routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,38 +3,24 @@ import ProjectsPage from "./pages/ProjectsPage";
 import ProjectDetails from "./pages/ProjectDetails";
 import BacklogDetails from "./pages/BacklogDetails";
 import ItemEdit from "./pages/ItemEdit";
-import ItemCreate from "./pages/ItemCreate"; // ✅ Import the new page
+import ItemCreate from "./pages/ItemCreate";
+import SprintDetails from "./pages/SprintDetails";
+import SprintItemCreate from "./pages/SprintItemCreate";
+import Index from "./pages/Index";
 
 const AppRoutes = () => {
     return (
         <Routes>
+            <Route path="/" element={<Index />} />
             <Route path="/projects" element={<ProjectsPage />} />
             <Route path="/projects/:id" element={<ProjectDetails />} />
             <Route path="/backlog/:id" element={<BacklogDetails />} />
             <Route path="/items/:id/edit" element={<ItemEdit />} />
-            <Route path="/projects/:id/items/create" element={<ItemCreate />} /> {/* ✅ Add the create route */}
+            <Route path="/projects/:id/items/create" element={<ItemCreate />} />
+            <Route path="/sprints/:id" element={<SprintDetails />} />
+            <Route path="/sprints/:id/items/create" element={<SprintItemCreate />} />
         </Routes>
     );
 };
 
 export default AppRoutes;
-
-
-// import { Routes, Route } from "react-router-dom";
-// import ProjectsPage from "./pages/ProjectsPage";
-// import ProjectDetails from "./pages/ProjectDetails";
-// import BacklogDetails from "./pages/BacklogDetails";
-// import ItemEdit from "./pages/ItemEdit"; // Import the Edit page
-
-// const AppRoutes = () => {
-//   return (
-//     <Routes>
-//       <Route path="/projects" element={<ProjectsPage />} />
-//       <Route path="/projects/:id" element={<ProjectDetails />} />
-//       <Route path="/backlog/:id" element={<BacklogDetails />} />
-//       <Route path="/items/:id/edit" element={<ItemEdit />} /> {/* ✅ New Edit Route */}
-//     </Routes>
-//   );
-// };
-
-// export default AppRoutes;
