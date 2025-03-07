969ec110ae78b227ecf86c4678cf875a39f25b37
sam-a-d <masamad.sust@gmail.com>
2025-03-07 12:07:50 +0200

    Index component added

---
+++ src/pages/Index.tsx
@@ -0,0 +1,18 @@
+const Index = () => {
+    return (
+        <>
+            <h1>🏠 Welcome to the Dashboard</h1>
+            <p>
+                This is a sample index page with some dummy text to test the layout.
+            </p>
+            <p>
+                You can navigate using the sidebar to explore different sections like Projects, Reports, and Settings.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla aliquet, libero at posuere sodales, lacus nunc tincidunt ligula, eget tristique orci enim et purus.
+            </p>
+        </>
+    );
+};
+
+export default Index;
