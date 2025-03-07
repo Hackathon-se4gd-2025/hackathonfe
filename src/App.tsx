b66177b50f760432694999904effaefa0edb5028
sam-a-d <masamad.sust@gmail.com>
2025-03-07 12:10:16 +0200

    modified app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,34 @@
 
 
 
+// import { BrowserRouter as Router } from "react-router-dom";
+// import AppRoutes from "./routes";
+
+// function App() {
+//   return (
+//     <Router>
+//       <AppRoutes />
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
 import { BrowserRouter as Router } from "react-router-dom";
+// import AppRoutes from "./AppRoutes";
 import AppRoutes from "./routes";
+// import Layout from "./components/Layout";
+import Layout from "./layouts/Layout";
 
 function App() {
-  return (
-    <Router>
-      <AppRoutes />
-    </Router>
-  );
+    return (
+        <Router>
+            <Layout>
+                <AppRoutes />
+            </Layout>
+        </Router>
+    );
 }
 
 export default App;
